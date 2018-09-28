3ab0b631217cad21a244d90f41aaa3eea3a951b9
Alfred Maldonado III <lowercasepeople49@gmail.com>
2018-09-28 19:22:18 -0400

    added js match logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again.");
+	};
+};
