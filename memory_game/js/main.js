449895d607ee245ca40efc43f606595aabb38c87
Alfred Maldonado III <lowercasepeople49@gmail.com>
2018-10-01 01:24:11 -0400

    finished

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,6 +21,15 @@ let cards = [
 	}
 ];
 var cardsInPlay = [];
+var createBoard = function () {
+	for (var cardsIndex = 0; cardsIndex < cards.length; cardsIndex++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', cardsIndex);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -28,18 +37,19 @@ var checkForMatch = function () {
 			alert("Sorry, try again.");
 	};
 }
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped ", cards[cardId].rank);
 	
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
-flipCard(0);
-flipCard(2);
+createBoard();
